791955919fc3847ed3e3dd1e554196b66eaf7d72
dgrushetskiy <dgrushetskiy@gmail.com>
2020-05-29 00:25:44 +0300

    add JpaDao and UserDao

---
+++ src/main/java/ru/gothmog/ws/dao/UserDao.java
@@ -0,0 +1,38 @@
+package ru.gothmog.ws.dao;
+
+import ru.gothmog.ws.entity.User;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class UserDao extends JpaDAO<User> implements GenericDao<User> {
+
+    public UserDao(EntityManager entityManager) {
+        super(entityManager);
+    }
+
+    @Override
+    public User update(User user) {
+        return null;
+    }
+
+    @Override
+    public User get(Object id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Object id) {
+
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+}
